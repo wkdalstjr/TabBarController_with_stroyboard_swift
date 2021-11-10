f0168140518cccb54b9d9dc31a7133337ecbc4d8
wkdalstjr <wkdalstjr1218@naver.com>
2021-11-10 14:46:49 +0900

    TabBarController with Storyboard Tutorial - swift

---
+++ TabBarController_swift/MyMainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarController_swift
+//
+//  Created by 장민석 on 2021/11/10.
+//  Copyright © 2021 장민석. All rights reserved.
+//
+
+import UIKit
+
+class MyMainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
